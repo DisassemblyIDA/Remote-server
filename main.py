8c1bda019b34bc9e14e1b99a9b0d4ea98c06db2d
DisassemblyIDA <139387920+DisassemblyIDA@users.noreply.github.com>
2024-10-30 09:22:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -50,7 +50,8 @@ real_nicknames = {
     "37.248.168.112": ["Lastik", True],
     "2.63.255.157": ["Manje", True],
     "37.248.221.146": ["Lastik", True],
-    "178.126.3.46": ["Zeref", True]
+    "178.126.3.46": ["Zeref", True],
+    "37.248.175.198": [Lastik", True]
 }   
 
 
